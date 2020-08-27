5f2da41aedba6b083be14e563aec269d6086d55c
Careytech <careythetech@gmail.com>
2020-08-27 15:56:38 -0400

    data

---
+++ backend/data.js
@@ -0,0 +1,104 @@
+export default  {
+    products: [
+    {
+        id: '1',
+        name: "Slim Hoodies",
+        category: 'Hoodie',
+        image: '/images/sportswear-mens-full-zip-hoodie-b0XPwr.jpg',
+        price: 40,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        id: '2',
+        name: " Nike Fit Shirt",
+        category: 'Shirt',
+        image: '/images/sportswear-mens-full-zip-hoodie-b0XPwr.jpg',
+        price: 50,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        id: '3',
+        name: "Nike Sweatpants",
+        category: 'Pants',
+        image: '/images/sportswear-mens-full-zip-hoodie-b0XPwr.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        id: '4',
+        name: "Slim Hoodies",
+        category: 'Hoodie',
+        image: '/images/sportswear-mens-full-zip-hoodie-b0XPwr.jpg',
+        price: 70,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        id: '5',
+        name: " Nike Fit Shirt",
+        category: 'Shirt',
+        image: '/images/sportswear-mens-full-zip-hoodie-b0XPwr.jpg',
+        price: 80,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        id: '6',
+        name: "Nike Sweatpants",
+        category: 'Pants',
+        image: '/images/sportswear-mens-full-zip-hoodie-b0XPwr.jpg',
+        price: 90,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        id: '7',
+        name: "Slim Hoodies",
+        category: 'Hoodie',
+        image: '/images/sportswear-mens-full-zip-hoodie-b0XPwr.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        id: '8',
+        name: " Nike Fit Shirt",
+        category: 'Shirt',
+        image: '/images/sportswear-mens-full-zip-hoodie-b0XPwr.jpg',
+        price: 70,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        id: '9',
+        name: "Nike Sweatpants",
+        category: 'Pants',
+        image: '/images/sportswear-mens-full-zip-hoodie-b0XPwr.jpg',
+        price: 80,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews:10
+    },
+    {
+        id: '10',
+        name: "Slim Hoodies",
+        category: 'Hoodie',
+        image: '/images/sportswear-mens-full-zip-hoodie-b0XPwr.jpg',
+        price: 90,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews:10
+    },
+]
+}
